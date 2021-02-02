211b9657ba362349392541d0151889bc45e7d000
szhshp-home <zillionleo@foxmail.com>
2021-02-02 23:38:52 +0800

    feat: save user Preferences and few fixes

--- src/renderer/store/mutations.js
+++ src/renderer/store/mutations.js
@@ -1,12 +1,17 @@
+import {STORAGE_KEY} from '../constants/index';
+
 const mutations = {
   setAPIConfig(state, data) {
     state.instance.experience.setAPIConfig(data);
+    localStorage.setItem(STORAGE_KEY.API_CONFIG, JSON.stringify(data));
   },
   setConfig: (state, data) => {
     state.instance.experience.setConfig(data);
+    localStorage.setItem(STORAGE_KEY.CONFIG, JSON.stringify(data));
   },
   setHotkey: (state, data) => {
     state.app.hotkey = data;
+    localStorage.setItem(STORAGE_KEY.HOTKEY, JSON.stringify(data));
   },
   setMessage: (state, data) => {
     state.app.message = data;
